09403d58bde1f56f45de444bf9c7961f752eb48c
santhosh37 <30381321+santhosh37@users.noreply.github.com>
2023-02-24 16:47:09 +0530

    1 file deleted

--- 1_session_(17_feb_2023)/index.js
+++
@@ -1,9 +0,0 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
-console.log(proccess.env.PWD);
-
-const fs = require('fs');
-fs.readFile('./hello.txt', 'utf-8', (error, data) => {
-  console.log(data);
-});
