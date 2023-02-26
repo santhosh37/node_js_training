a4679449da2ee0a276ab705282e6e05cab92bbba
santhosh37 <30381321+santhosh37@users.noreply.github.com>
2023-02-26 22:56:28 +0530

    1 file modified, 1 added

--- 5_session_23_feb_2023/index.js
+++ 5_session_23_feb_2023/index.js
@@ -1,9 +1,22 @@
 const http = require('http');
 const port = 8080;
+const { getUrl } = require('utils');
 
 http
   .createServer(function (request, response) {
     console.log('server created with port ', port);
+    console.log(request.url);
+    const url = getUrl(request.url);
+
+    console.log(url);
+
+    if (request.url === '/file1') {
+      // do somthing
+    } else if (request.url === '/file2') {
+      // do somthing
+    } else {
+      // else throw error
+    }
     response.writeHead(200, { 'content-type': 'text/html' });
     response.write('hello santhosh');
     response.end();
---
+++ 5_session_23_feb_2023/utils.js
@@ -0,0 +1,6 @@
+const getUrl = (url) => {
+  // return url.replace('/','');
+  return url.split('/')[1];
+};
+
+module.exports = { getUrl };
