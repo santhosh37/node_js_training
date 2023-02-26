3dc5f4f76a45aacee674af1387bc59df3c22a7a5
santhosh37 <santhoshcse037@gmail.com>
2023-02-27 00:13:31 +0530

    dummy changes

--- 5_session_23_feb_2023/index.js
+++ 5_session_23_feb_2023/index.js
@@ -1,23 +1,22 @@
-const http = require('http');
+const http = require("http");
 const port = 8080;
-const { getUrl } = require('./utils');
+const { getUrl } = require("./utils");
 
 http
   .createServer(function (request, response) {
-    console.log('server created with port ', port);
+    console.log("server created with port ", port);
     console.log(request.url);
     const url = getUrl(request.url);
 
     console.log(url);
 
-
     // switch approch
     switch (url) {
-      case 'file1':
+      case "file1":
         //do something
         break;
 
-      case 'file2':
+      case "file2":
         //do something
         break;
 
@@ -26,15 +25,15 @@ http
     }
 
     // if approch
-    if (url === '/file1') {
+    if (url === "/file1") {
       // do somthing
-    } else if (url === '/file2') {
+    } else if (url === "/file2") {
       // do somthing
     } else {
       // else throw error
     }
-    response.writeHead(200, { 'content-type': 'text/html' });
-    response.write('hello santhosh');
+    response.writeHead(200, { "content-type": "text/html" });
+    response.write("hello santhosh");
     response.end();
   })
   .listen(port);
